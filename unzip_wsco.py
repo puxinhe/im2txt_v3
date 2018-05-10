b0767cb42f6c7eddbc7223d4af94e52228f3ce3d
HataFeng <fbjwying2@163.com>
2018-05-10 13:46:21 +0800

    test

--- unzip_wsco.py
+++ unzip_wsco.py
@@ -1,28 +1,13 @@
 import zipfile
 import os
 
-import argparse
-
 def un_zip(from_file_name, to_file_dir):
   """unzip zip file"""
   zip_file = zipfile.ZipFile(from_file_name)
-  if os.path.isdir(to_file_dir):
-    pass
-  else:
-    os.mkdir(to_file_dir)
-
   for names in zip_file.namelist():
     zip_file.extract(to_file_dir)
 
   zip_file.close()
-
-
-def parse_args(check=True):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--output_dir', type=str, required=True)
-    parser.add_argument('--dataset_dir', type=str, required=True)
-    FLAGS, unparsed = parser.parse_known_args()
-    return FLAGS, unparsed
 
 if __name__ == "__main__":
   print('current working dir [{0}]'.format(os.getcwd()))
@@ -30,9 +15,5 @@ if __name__ == "__main__":
   print('change wording dir to [{0}]'.format(w_d))
   os.chdir(w_d)
 
-  FLAGS, unparsed = parse_args()
-  DIR_TO_TEST_DATA = os.path.join(FLAGS.output_dir, 'test_data/')
-  PATH_TO_TEST_ZIP = os.path.join(FLAGS.dataset_dir, 'test.zip')
-
-  un_zip(PATH_TO_TEST_ZIP, DIR_TO_TEST_DATA)
+  un_zip('/data/HataFeng/data-im2txt/test.zip', 'output/')
 
